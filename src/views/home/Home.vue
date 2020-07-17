e2690760f6e7438eae63da9229345890ea77bb81
万福涛 <wanfutao11@163.com>
2020-07-17 20:09:29 +0800

    master

---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+       name: '', 
+    }
+</script>
+
+<style lang= 'scss' scoped>
+    
+</style>
